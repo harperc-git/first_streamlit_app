cac5ea50442a7bde5765763c232d05bb220dd295
harperc-git <57406381+harperc-git@users.noreply.github.com>
2022-09-07 16:26:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,3 @@ streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-🥗 🐔 🥑🍞
