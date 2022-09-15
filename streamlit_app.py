899886c0fb33c9c102251c86fc3135988908a273
harperc-git <57406381+harperc-git@users.noreply.github.com>
2022-09-15 18:13:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,5 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
